9b1e3e3a0013bb75e285b59aa7e84e2eb4948e73
Zhenya Kudrya <megusta997@gmail.com>
2023-09-18 19:12:57 +0300

    add tittle_func and docstring

--- str_func.py
+++ str_func.py
@@ -1,4 +1,15 @@
 def upp_func(value):
+    """
+    Делает заглавными все буквы
+    """
     new_value = value.upper()
     return new_value
 
+def tittle_func(value):
+    """
+    Делает заглавными первые буквы
+    каждого слова в строке,
+    поступившей на вход функции
+    """
+    new_value = value.tittle()
+    return new_value
